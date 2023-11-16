3c739e3ae9ce5e6742da000cf0193f8f246caf49
livingod <144990631+livingod@users.noreply.github.com>
2023-11-16 16:09:00 +0900

    05

--- main.c
+++ main.c
@@ -3,24 +3,22 @@
 
 int main(int argc, char *argv[])
 {
-   int i;
-   int grade[5];
-   int average = 0;
+   char *pc;
+   int *pi;
+   double *pd;
    
-   for(i=0;i<5;i++)
-   {
-                   printf("input value [%i]= ",i);
-                   scanf("%d",&grade[i]);
-   }
+   pc = (char*)10000;
+   pi = (int*)10000;
+   pd = (double*)10000;
    
-   for(i=0;i<5;i++)
-   {
-                   printf("grade[%i] = %i\n", i, *(grade+i));//grade+i ???????
-                   average = average + *(grade+i)/5;
-   }
-                   
-   printf("average = %i\n", average);
+   printf("증가 전: pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
    
+   pc++;
+   pd++;
+   pi++;
+   
+   printf("증가 전: pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
+    
   system("PAUSE");	
   return 0;
 }
